7db4f9566ec8a5d172696085d9a6e44f4e4d4ca8
Moritz <moritzzimmer@moritzzimmer.com>
2018-10-14 09:26:51 -0500

    Re-worked for Safari
    Re-wrote large portions of the app to make it work with all mobile browsers, especially pertaining to the address bar.

--- flipbookyfy.js
+++ flipbookyfy.js
@@ -6,24 +6,39 @@ var flipbookyfy = (function($){
    *
    *  <div class="main">
    *    <article>
-   *      <div class="header"></div>
+   *      <div class="header"> <a href="some-page1.html">Some link1</a> </div>
    *      <div class="summary"></div>
    *    </article>
    *    <article>
-   *      <div class="header"></div>
+   *      <div class="header"> <a href="some-page2.html">Some link2</a> </div>
    *      <div class="summary"></div>
    *    </article>
+   *    […]
    *  <div>
    *
-   * ATTENTION!!!!!: The wrapper for (<article> in the example) will be removed by the script!
-   * Make sure you don't rely on them in your CSS.
+   * ATTENTION!!!!!:
    *
-   * @param  {string} containerclass The class containing the items that should be flipped (.main in the example markup)
-   * @param  {string} headerclass    The class of the top portion of the flipbook (.header) in the example
-   * @param  {string} summaryclass   The class of the bottom portion of the flipbook (.summary) in the example
-   * @param  {array} excludeitems    An array of classnames (strings) you wish to exclude
-   * @param  {string} heightmodifier EXPERIMENTAL!!!! If you want to slow down or speed up the scroll.
-   *                                 For example: Pass 0.5 for twice the speed, 2 for half. Default is 1.
+   *   The wrapper for (<article> in the example) will be removed by the script!
+   *   Make sure you don't rely on them in your CSS.
+   *
+   *   If your markup has any links in it, make sure they are in the header portion. The script will
+   *   look for the first link in the header and use it for each entire flipcard application state
+   *
+   * ARGUMENTS
+   *
+   *   You will need to pass a Javascript object as an argument, but within that object,
+   *   you have these parameters
+   *
+   *   MANDATORY
+   *   @param  {string} containerclass The class containing the items that should be flipped (.main in the example markup)
+   *   @param  {string} headerclass    The class of the top portion of the flipbook (.header) in the example
+   *   @param  {string} summaryclass   The class of the bottom portion of the flipbook (.summary) in the example
+   *
+   *   OPTIONAL
+   *   @param  {array}  excludeitems   An array of classnames (strings) you wish to exclude
+   *   @param  {int}    perspective    Defauly is 0, Don't use px. The CSS perspective on the flipping cards.
+   *   @param  {string} heightmodifier EXPERIMENTAL!!!! If you want to slow down or speed up the scroll.
+   *                                   For example: Pass 0.5 for twice the speed, 2 for half. Default is 1.
    */
   var _flipbookyfy = function( input ){
 
@@ -63,7 +78,7 @@ var flipbookyfy = (function($){
     }
     // defaults
     heightmodifier = heightmodifier || 1;
-    perspective = perspective || '0px';
+    perspective = perspective || '0';
 
     // prepares neccesary classes and styles for the flipbook
     var headstyles =
@@ -75,8 +90,6 @@ var flipbookyfy = (function($){
           'width: 100%;'+
           'height: 50%;'+
         '}'+
-// next header current summary
-
         containerclass +' '+ headerclass +  ':not(.this--nextitem), '+
         containerclass +' '+ summaryclass + ':not(.this--isinview){ '+
           'transform: rotateX(0deg) !important;' +
@@ -96,6 +109,23 @@ var flipbookyfy = (function($){
         '.this--isinview'+headerclass+'{ z-index: 2001 !important; }' +
         '.this--nextitem'+headerclass+'{ z-index: 2002 !important; }' +
         '.this--nextitem'+summaryclass+'{ transform: rotateX(0deg) !important; }' +
+        '.filpbookscrollplaceholder{' +
+          'top: 0; ' +
+          'display: block; ' +
+          'position: relative; ' +
+          'width: 100%; ' +
+          'z-index: 4998;' +
+          'opacity: 0.7; ' +
+          // For any overlays over the flipbook application, use the same kinda logic
+          // in your other CSS as follows (needed for safari browsers):
+          // Since your flipping elements will live in a perspective state, you will need ANYTHING
+          // that overlays them to also live in the same state AND be “obove” them,
+          // What it does here is move it up on the Z Axis by half the perspective, bringing
+          // it 2x closer to the virtual camera, which means we will need to render the item
+          // at half the size in order to make it appear flat and fit into it's original space
+          'transform: perspective('+perspective+'px) translateZ('+perspective/2+'px) scale(0.5);' +
+          'transform-style: preserve-3d;' +
+        '}' +
       '</style>';
     $('head').append( headstyles );
 
@@ -106,17 +136,17 @@ var flipbookyfy = (function($){
     var _prepDoc = function(){
 
       windowheight = $(window).height()*heightmodifier - excludedheight;
+      // leaving these here for reference for now.
       // windowheight = screen.availHeight*heightmodifier - excludedheight;
       // windowheight = window.innerHeight*heightmodifier - excludedheight;
-
 
       $childelements = $(containerclass).children();
       childnumber   = $childelements.length;
 
       // applies reverse order z-indices.
-      for(var i=0; i<childnumber; i++){
-        var $curset = $($childelements[i]);
-        $curset.children().addClass('elementnumber'+i).css('zIndex', (childnumber-i)*100 );
+      for(var h=0; h<childnumber; h++){
+        var $curset = $($childelements[h]);
+        $curset.children().addClass('elementnumber'+h).css('zIndex', (childnumber-h)*100 );
       }
 
       // removes wrapper ('article')
@@ -127,21 +157,63 @@ var flipbookyfy = (function($){
       // that would be scrolled if all the elements were to
       // render in a row in stead of on top of each other.
       $(containerclass).children().css('height', windowheight/(2*heightmodifier)+'px');
-      $(containerclass).css('height', windowheight*childnumber +'px');
+      $(containerclass).css('height', windowheight*childnumber +'px').css('overflow', 'hidden');
 
       // offsets the second element at the correct height
       $(containerclass).find(summaryclass).css('top', windowheight/(2*heightmodifier)+'px');
+
+      // wraps the entire (overflow: hidden) flipbook application
+      // in an overflow: scroll container, only adjust height if already exists
+      if( !$('.flipbookwrapper').length ){
+        $(containerclass).wrap('<div class="flipbookwrapper" style="height: '+windowheight+'px; overflow: scroll; -webkit-overflow-scrolling: touch;"></div>');
+      }else{
+        $('.flipbookwrapper').css('height', windowheight+'px');
+      }
+
     };
 
+    /**
+     * creates an artifical scroll and click interface.
+     * Here is where it important that you put at least one link
+     * in the Header markup, because this will find it and
+     * use it as the link in the click overlay
+     * Does not run (hopefully) if already exists, but if it does
+     * It will adjust the height of the element accordingly
+     */
+    var _initDocOnly = function(){
+      if( !$('.filpbookscrollplaceholder').length ){
+        for(var i=0; i<childnumber; i++){
+          var $allHeaders = $(headerclass) ;
+          var $currHeader = $($allHeaders[i]);
+          var currentLink = $currHeader.find('a').attr('href');
+          var startMarkup = '<div ';
+          var endMarkup   = 'div>';
+
+          if(currentLink.length){
+            startMarkup = '<a href="'+currentLink+'" ';
+            endMarkup   = 'a>';
+          }
+          $(containerclass).append(startMarkup + ' class="filpbookscrollplaceholder" style="height:' + windowheight + 'px"></' + endMarkup);
+        }
+      }else{
+        for(var b=0; b<childnumber; b++){
+          var $allScrollPlaceholders = $('.filpbookscrollplaceholder');
+          var $curScrollPlaceholder  = $($allScrollPlaceholders[b]);
+          $curScrollPlaceholder.css('height', windowheight + 'px');
+        }
+      }
+    };
 
     _prepDoc();
+    _initDocOnly();
     $( window ).resize(function() {
       _prepDoc();
     });
 
-    $(window).scroll( function(){
+    // listens for scroll and does all the magic
+    $('.flipbookwrapper').scroll( function(){
 
-      var wnps = $(window).scrollTop();
+      var wnps = $('.flipbookwrapper').scrollTop();
       wnps = wnps/heightmodifier;
 
       // resets every time the height of one element has been scrolled
@@ -175,12 +247,12 @@ var flipbookyfy = (function($){
           $nextSummary.addClass('this--nextitem').removeClass('this--previtem');
 
           if( degree >= 90 ){
-            $nextHeader.css('transform', 'perspective('+perspective+') rotateX('+((degree-180))+'deg)');
-            $curSummary.css('transform', 'perspective('+perspective+') rotateX('+90+'deg)');
+            $nextHeader.css('transform', 'perspective('+perspective+'px) rotateX('+((degree-180))+'deg)');
+            $curSummary.css('transform', 'perspective('+perspective+'px) rotateX('+90+'deg)');
           }else{
-            $curSummary.css('transform', 'perspective('+perspective+') rotateX('+degree+'deg)');
-            $curHeader.css('transform', 'perspective('+perspective+') rotateX('+0+'deg)');
-            $nextHeader.css('transform', 'perspective('+perspective+') rotateX('+90+'deg)');
+            $curSummary.css('transform', 'perspective('+perspective+'px) rotateX('+degree+'deg)');
+            $curHeader.css('transform', 'perspective('+perspective+'px) rotateX('+0+'deg)');
+            $nextHeader.css('transform', 'perspective('+perspective+'px) rotateX('+90+'deg)');
           }
         }
         // if element is no longer in view, and related logic.
@@ -203,10 +275,8 @@ var flipbookyfy = (function($){
 
     _flipbookyfy(input);
 
-    // window.addEventListener('resize', function(){
-    //   flipbookyfy(input);
-    // });
-
+    // hack needed in order to start drawing
+    // all the elements correctly after load
     window.scrollBy(0, 1);
     window.scrollBy(0, -1);
 
